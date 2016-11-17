f55b25d307426c1a89b3df33c6fc5e436e66c3fe
Parish <lparish@deloitte.com>
2016-11-17 17:24:08 -0500

    search module not showing partial

--- app.js
+++ app.js
@@ -16,19 +16,30 @@ var apos = require('apostrophe')({
     },
     
     // Standard Apostrophe Modules
+    'apostrophe-search': {},
     'apostrophe-assets': {},
     'apostrophe-blog': {},
     'apostrophe-blog-pages': {},
     'apostrophe-blog-widgets': {},
     'apostrophe-users': {},
-
+    'apostrophe-pages': {
+          // other configuration, then ...
+          park: [
+            {
+              slug: '/search',
+              type: 'apostrophe-search',
+              label: 'Search',
+              published: true
+            }
+          ]
+        },
     // Apostrophe Sandbox (as-) specific modules
     'as-helpers': {},
-    'as-two-column-block-widgets': {},
+    'as-two-column-block-widgets': {}
 
     // REMOVE ME IMMEDIATELY if you are not running a public demo
     // that should let EVERYBODY be INSTANTLY loggged in AS ADMIN!
-    'demo-autologin': {},
+    //'demo-autologin': {},
 
   }
 
